7647fda92048a433e579459136059489075d0e5a
SinTan1729 <sayantan.santra689@gmail.com>
2022-01-02 22:35:07 -0600

    Cleanup and commentary

--- start-font-fix.sh
+++ start-font-fix.sh
@@ -1,7 +1,7 @@
+#! /bin/sh
+
 # This was to fix an issue I was having where my terminal fonts were all over the place and tlp was autosuspending usb ports at startup.
 # Anyone else probably won't need it.
-
-#! /bin/sh
 
 source ~/.profile
 sudo ~/.local/bin/personal/usb-autosuspend-fix
