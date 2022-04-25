9781a32a72d484abe7bfa1f1a7cc865b3587c3b9
SinTan1729 <sayantan.santra689@gmail.com>
2022-04-25 11:14:11 -0500

    deleted start-font-fix.sh

--- start-font-fix.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-# This was to fix an issue I was having where my terminal fonts were all over the place and tlp was autosuspending usb ports at startup.
-# Anyone else probably won't need it.
-
-source ~/.profile
-sudo ~/.local/bin/personal/usb-autosuspend-fix
