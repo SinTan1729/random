afe8218705f3869e9ec20c9f60a6a23e1dec9492
SinTan1729 <sayantan.santra689@gmail.com>
2022-06-26 19:22:50 -0500

    Modifed to better work with crontab

---
+++ brightness-by-daylight.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# This is a script to set my external monitor's brightness
+# to high (70%) or low (40%) automatically according to day/night.
+# It uses ddcutil to access the monitor's settings and sunwait to know
+# if it's day or night. It expects a file in the following format in
+# the .config directory (the values should be according to location)
+
+# ~/.config/latlong.toml
+# latitude="0.000000N"
+# longitude="0.000000E"
+
+# Add the following to crontab to make it run automatically and properly
+
+# @reboot <location-of-this-script>
+# 0 0,12 * * * <location-of-this-script> crontab
+
+confdir="/home/sintan/.config"
+
+if test -f $confdir/latlong.toml ; then
+    source $confdir/latlong.toml
+else
+    echo "No location config found!"
+fi
+
+# Check if called by crontab. If yes, start waiting, otherwise
+# adjust brightness immediately
+if [ "$1" == "crontab" ]; then
+    if [ $(sunwait poll $latitude $longitude) == "DAY" ]; then
+        sunwait wait set offset 10 $latitude $longitude && ddcutil setvcp 10 40
+    else
+        sunwait wait rise offset 10 $latitude $longitude && ddcutil setvcp 10 70
+    fi
+else
+    if [ $(sunwait poll $latitude $longitude) == "DAY" ]; then
+        ddcutil setvcp 10 70
+        echo "Monitor brightness set to 70%, since it's day time."
+    else
+        ddcutil setvcp 10 40
+        "Monitor brightness set to 40%, since it's night time."
+    fi
+fi
