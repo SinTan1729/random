cef815008423d52df531a68ea8f6daec8d21e2ee
SinTan1729 <sayantan.santra689@gmail.com>
2022-07-30 02:26:09 -0500

    Modify destination options

--- push_script_to.sh
+++ push_script_to.sh
@@ -6,9 +6,11 @@
 if [ -z "$1" ]; then
     echo "Provide a script or pass --help to see syntax"
     exit
+
 elif [ "$1" == "--help" ];then
     echo "The syntax is as follows:"$'\n'"push_script_to <script-location> <destination> (additional destinations separated by space)"
     exit
+
 elif ! [ -f "$1" ]; then
     echo "The given script doesn't exist"
     exit
@@ -18,7 +20,7 @@ fi
 
 for i in "${@:2}"
 do
-    if [ "$i" == "local_script_dir" ]; then
+    if [ "$i" == "personal_script_dir" ]; then
         cp "$1" "/home/sintan/.local/bin/personal/"
         chmod +x "/home/sintan/.local/bin/personal/$(basename "$1")"
 
@@ -26,9 +28,13 @@ do
         sudo cp "$1" "/etc/cron.daily/"
         sudo chmod +x "/etc/cron.daily/$(basename "$1")"
         
-    elif [ "$i" == "cron_weeky" ]; then
+    elif [ "$i" == "cron_weekly" ]; then
         sudo cp "$1" "/etc/cron.weekly/"
         sudo chmod +x "/etc/cron.weekly/$(basename "$1")"
+
+    elif [ "$i" == "cron_monthly" ]; then
+        sudo cp "$1" "/etc/cron.monthly/"
+        sudo chmod +x "/etc/cron.monthly/$(basename "$1")"
 
     else echo "Unrecognized destination: $i"
     fi
