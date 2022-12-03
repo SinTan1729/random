44f5845caf12e0509a86805b1665eb8de84cb5e2
SinTan1729 <sayantan.santra689@gmail.com>
2022-12-03 03:09:23 -0600

    Various improvements

--- lure-update.sh
+++ lure-update.sh
@@ -4,17 +4,20 @@
 # The regular upgrade function of lure doesn't suffice since
 # I use automatically updating version names in my install scripts
 
-[ -f "$1" ] || echo "Please pass location of a list with github repo names"
+! command -v lure>/dev/null && echo "lure is not installed" && exit
+! [ -f "$1" ] && echo "Please pass location of a list with github repo names" && exit
+
+lure_list=$(lure list --installed 2>/dev/null | grep -v 'INF')
 
 for f in $(cat "$1" | tr '\n' ' ')
 do
 	pkg="$(echo $f | cut -d '/' -f 2)"
-	version_new=$(lure info $pkg | grep version | awk '{printf $2}' | sed 's/[a-zA-Z]*//g')
+	version_new=$(lure info $pkg 2>/dev/null | grep version | awk '{printf $2}' | sed 's/[a-zA-Z]*//g')
 	[ "$version_new" == "" ] && version_new="$(curl -sL "https://api.github.com/repos/${f}/releases/latest" | jq -r '.tag_name' | sed 's/[a-zA-Z]*//g')"
-	version_present="$(dnf info $pkg 2>/dev/null | grep Version | awk '{printf $3}' | sed 's/[a-zA-Z]*//g')"
+	version_present=$(echo "$lure_list" | grep $pkg | cut -d' ' -f2 | cut -d- -f1 | sed 's/[a-zA-Z]*//g')
 	[ "$version_present" == "" ] && echo "$pkg isn't installed, skipping" && continue
 	if [ $(echo $version_present$'\n'$version_new | sort -V | tail -n1) != $version_present ]; then
-		echo "Upgrading $pkg"
+		echo "Upgrading $pkg ($version_present -> $version_new)"
 		lure install $pkg
 	else
 		echo "$pkg is up-to-date"
