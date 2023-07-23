a3b2f5a6893acd7a6919278fed550ab2885cec0b
SinTan1729 <sayantan.santra689@gmail.com>
2023-07-23 18:22:35 -0500

    fix: LURE updates work now

--- update-aur-lure.sh
+++ update-aur-lure.sh
@@ -25,7 +25,7 @@ updpkgsums
 makepkg --printsrcinfo >.SRCINFO
 
 # Remove downloaded files
-ls | grep -v PKGBUILD | xargs -r -I {} rm -i "{}"
+ls | grep -v PKGBUILD | xargs -r -I {} rm "{}"
 git add .
 git commit -m "Bumped $PKG version to $VERS"
 git push
@@ -33,21 +33,21 @@ git push
 # Update the GitHub backup repo as well
 echo "Updating AUR backup repo..."
 cd "$GIT_DIR/AUR Mirror GitHub/$PKG"
-sed -i -E "s/pkgver=[0-9\.]+/pkgver=$VERS/" PKGBUILD
+sed -i -E "s/version=[0-9\.]+/version=$VERS/" PKGBUILD
 git add .
 git commit -m "Bumped $PKG version to $VERS"
 git push
 
 # Do the updates for LURE
 echo "Updating LURE repo..."
+cd "$GIT_DIR/lure-repo/"
 [ -d "$1" ] && PKG="$1" || PKG="$1-bin"
-
-cd "$GIT_DIR/lure-repo/$PKG"
+cd "$PKG"
 sed -i -E "s/pkgver=[0-9\.]+/pkgver=$VERS/" lure.sh
 update-lure-checksums.py
 
 # Remove downloaded files
-ls | grep -v lure.sh | xargs -r -I {} rm -i "{}"
+ls | grep -v lure.sh | xargs -r -I {} rm "{}"
 git add .
 git commit -m "Bumped $PKG version to $VERS"
 git push
