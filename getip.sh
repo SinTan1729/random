39cfca0e6be1bc88b8a0a6815395eb8daf8154af
SinTan1729 <sayantan.santra689@gmail.com>
2023-08-02 17:24:42 -0500

    new: Add more interface names

--- getip.sh
+++ getip.sh
@@ -3,7 +3,7 @@
 # This is a simple script to get the ip addresses of a machine.
 
 echo 'ipv4:'
-for i in eno1 wlan0; do
+for i in eth0 eno1 wlan0; do
     $(ip -4 addr | grep -q $i) || continue
     addr="$(ip -4 addr show dev $i | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
     [ -z "$addr" ] || echo -e "\t$i: \t\t$addr"
@@ -14,7 +14,7 @@ echo -e "\texternal: \t${addr:-n/a}"
 [ "$(cat /sys/module/ipv6/parameters/disable)" == "1" ] && exit
 
 echo 'ipv6:'
-for i in eno1 wlan0; do
+for i in eth0 eno1 wlan0; do
     $(ip -6 addr | grep -q $i) || continue
     addr="$(ip -6 addr show dev $i | grep -oP '(?<=inet6\s)\w+(\:{1,2}\w+){4}(?=.+link)')"
     [ -z "$addr" ] || echo -e "\t$i: \t\t$addr"
