a6318c9144cf1f6a831a5ea595808f3cd16b2a86
SinTan1729 <sayantan.santra689@gmail.com>
2024-12-09 23:40:07 -0600

    fix: Works with cron properly

--- qbt-set-forwarded-port.sh
+++ qbt-set-forwarded-port.sh
@@ -9,8 +9,9 @@ set -e
 qbt_username="${QBT_USERNAME:-admin}"
 qbt_password="${QBT_PASSWORD:-adminadmin}"
 qbt_addr="${QBT_ADDR:-http://localhost:8085}" # ex. http://10.0.1.48:8080
+gluetun_container="${GLUETUN_COMTAINER:-'qbt-gluetun'}"
 
-port_number=$(docker exec -it qbt-gluetun cat /tmp/gluetun/forwarded_port)
+port_number=$(docker exec "$gluetun_container" cat /tmp/gluetun/forwarded_port)
 
 if [ -z "$port_number" ]; then
     echo "Could not figure out which port to set."
