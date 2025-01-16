1e246eb2fea0ead6936aa36a64e3d211c9d11974
SinTan1729 <sayantan.santra689@gmail.com>
2025-01-16 15:46:01 -0600

    chg: Added muting

--- DNDToggle.sh
+++ DNDToggle.sh
@@ -6,6 +6,8 @@ dnd=$(pgrep DND.py)
 if [[ $dnd ]];
 then
     kill $dnd
+    pactl set-sink-mute @DEFAULT_SINK@ false
 else
 	/home/sintan/.local/bin/personal/DND.py &
+    pactl set-sink-mute @DEFAULT_SINK@ true
 fi
