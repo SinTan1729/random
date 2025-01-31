83976405d912d2651505f2382f8f6c9821896e61
SinTan1729 <sayantan.santra689@gmail.com>
2025-01-30 19:55:16 -0600

    new: Added open-in-server.sh

---
+++ open-in-server.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This script opens supported locations in my server by appropriately changing the
+# mount locations for it to work over ssh.
+
+ROOTDIR="$(echo $PWD | cut -d/ -f-2,3)"
+
+if [ "$ROOTDIR" == "/mnt/server" ]; then
+    ssh -t server-ts-rsync "cd /mnt/storage/$(echo $PWD | cut -d/ -f4-); fish -l"
+elif [[ "$ROOTDIR" == "/tank1/personal" || "$ROOTDIR" == "/tank1/media" ]]; then
+    ssh -t server-ts-rsync "cd $PWD; fish -l"
+else
+    echo "Unsupported directory."
+    echo "Only /mnt/server/*, /tank1/* are supported."
+fi
+
