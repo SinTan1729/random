99a494ea1e5e58b81771b629079f6dc0d447fb1a
SinTan1729 <sayantan.santra689@gmail.com>
2025-01-31 01:41:37 -0600

    fix: Wrap paths in quotes

--- open-in-server.sh
+++ open-in-server.sh
@@ -6,9 +6,9 @@
 ROOTDIR="$(echo $PWD | cut -d/ -f-2,3)"
 
 if [ "$ROOTDIR" == "/mnt/server" ]; then
-    ssh -t server-ts-rsync "cd /mnt/storage/$(echo $PWD | cut -d/ -f4-); fish -l"
+    ssh -t server-ts-rsync "cd \"/mnt/storage/$(echo $PWD | cut -d/ -f4-)\"; fish -l"
 elif [[ "$ROOTDIR" == "/tank1/personal" || "$ROOTDIR" == "/tank1/media" ]]; then
-    ssh -t server-ts-rsync "cd $PWD; fish -l"
+    ssh -t server-ts-rsync "cd \"$PWD\"; fish -l"
 else
     echo "Unsupported directory."
     echo "Only /mnt/server/*, /tank1/* are supported."
